f4add0514167de6d4591bd86f2451fc6f7687294
ELginas <gintaras.z123@yahoo.com>
2025-02-21 23:07:22 +0200

    inventory <-> cursor item sync and bit of timeline fix

--- cursor_item.gd
+++ cursor_item.gd
@@ -1,12 +1,30 @@
 extends TextureButton
 
+var inventory: Inventory
+
+func _ready():
+	inventory = get_tree().get_first_node_in_group("Inventory")
+
 func _process(delta: float) -> void:
 	position = get_global_mouse_position()
 
 func enter(item):
-	get_node(item).visible = true
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	if not is_interactable(item):
+		return
+	if item == "MagnifyingGlass":
+		get_node(item).visible = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func exit(item):
-	get_node(item).visible = false
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if not is_interactable(item):
+		return
+	if item == "MagnifyingGlass":
+		get_node(item).visible = false
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func is_interactable(item):
+	if not inventory.selected_item and item == "MagnifyingGlass":
+		return true
+	if inventory.selected_item and item == inventory.selected_item.item_name:
+		return true
+	return false
--- scenes/item.gd
+++ scenes/item.gd
@@ -7,12 +7,18 @@ func _ready() -> void:
 	CURSOR_ITEM = get_tree().get_first_node_in_group("CursorItem")
 
 func _on_mouse_entered() -> void:
+	if not CURSOR_ITEM.is_interactable(item_type):
+		return
 	CURSOR_ITEM.enter(item_type)
 
 func _on_mouse_exited() -> void:
+	if not CURSOR_ITEM.is_interactable(item_type):
+		return
 	CURSOR_ITEM.exit(item_type)
 
 func _on_pressed() -> void:
+	if not CURSOR_ITEM.is_interactable(item_type):
+		return
 	GameState.unpause()
 	visible = false
 	CURSOR_ITEM.exit(item_type)
